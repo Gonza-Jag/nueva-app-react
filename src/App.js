f68f9bc235e472052db323e2494ca4687c43099d
GonzaJag <gonzalogodoy22@outlook.com>
2022-04-16 11:03:49 -0300

    primer-desafio

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><a></a>Inicio</li>
+          <li><a></a>Contacto</li>
+          <li><a></a>Tienda</li>
+        </ul>
+      </nav>
+      <div>
+       <h1>Godoy Gonzalo/ Bienvenidos a nueva.app</h1>
+       <input></input>
+       
+       
+      </div>
+      
+    </header>
+
+  </div>
+ 
+  );
+}
+
+export default App;
